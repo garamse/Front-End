12a7527567a9ee312d1f4ea9bf55fea5c4d6d293
garam.kim <bd64118@naver.com>
2023-05-10 17:53:15 +0900

    20230510

---
+++ React/20230510/reactform/src/App.js
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import EventForm from "./components/EventForm";
+import ItemGenerator from "./components/ItemGenerator";
+
+function App() {
+
+  const [datas, setDatas] = useState({});
+
+  const addData = (data) => {
+    setDatas([...datas, data]);
+  }
+
+  return (
+    <div>
+      <ItemGenerator />
+      <EventForm addData={addData} />
+    </div>
+  );
+}
+export default App;
